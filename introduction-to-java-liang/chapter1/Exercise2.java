e481c82b9a221aaf8f552fe437dc941b281d19c4
jkcn90 <jkcn90@outlook.com>
2013-02-03 01:08:40 -0500

    Added chapter1.Exercise2

---
+++ introduction-to-java-liang/chapter1/Exercise2.java
@@ -0,0 +1,16 @@
+package chapter1;
+/**
+ * Display "Welcome to Java" five times.
+ */
+
+public class Exercise2
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Welcome to Java");
+    System.out.println("Welcome to Java");
+    System.out.println("Welcome to Java");
+    System.out.println("Welcome to Java");
+    System.out.println("Welcome to Java");
+  }
+}
